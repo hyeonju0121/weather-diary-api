933053ca9c03b7246576b4c1abbc664f89690031
yu hyun ju <67223214+hyeonju0121@users.noreply.github.com>
2023-07-21 01:18:22 +0900

    Feat : DiaryService parseWeather(), createDiary() 구현
    - 날씨 데이터 json 파싱 작업 후, Map 으로 파싱된 데이터를 return 함
    - createDiary() 일기 작성하는 메서드 구현 완료

--- src/main/java/zerobase/weather/service/DiaryService.java
+++ src/main/java/zerobase/weather/service/DiaryService.java
@@ -1,13 +1,21 @@
 package zerobase.weather.service;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import zerobase.weather.domain.Diary;
+import zerobase.weather.repository.DiaryRepository;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class DiaryService {
@@ -15,18 +23,32 @@ public class DiaryService {
     @Value("${openweathermap.key}")
     private String apiKey;
 
+    private final DiaryRepository diaryRepository;
+
+    public DiaryService(DiaryRepository diaryRepository) {
+        this.diaryRepository = diaryRepository;
+    }
+
     public void createDiary(LocalDate date, String text) {
         // 1. open weather map 에서 날씨 데이터 가져오기
         String weatherData = getWeatherString();
 
         // 2. 받아온 날씨 json 파싱하기
+        Map<String, Object> parseWeather = parseWeather(weatherData);
 
         // 3. 파싱된 데이터 + 작성한 일기 값을 DB 에 저장하기
+        Diary nowDiary = new Diary();
+        nowDiary.setWeather(parseWeather.get("main").toString());
+        nowDiary.setIcon(parseWeather.get("icon").toString());
+        nowDiary.setTemperature((Double) parseWeather.get("temp"));
+        nowDiary.setText(text);
+        nowDiary.setDate(date);
+
+        diaryRepository.save(nowDiary);
     }
 
-
     /**
-     * open weather map 에서 데이터를 String 으로 받아오기
+     * open weather map 에서 데이터를 String 으로 받아오는 메서드
      */
     private String getWeatherString() {
         String apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=seoul&appid=" + apiKey;
@@ -55,5 +77,32 @@ public class DiaryService {
         } catch (Exception e) {
             return "failed to get response";
         }
+    }
+
+    /**
+     * 받아온 날씨 데이터를 JSON 파싱하는 메서드
+     */
+    private Map<String, Object> parseWeather(String jsonString) {
+        JSONParser jsonParser = new JSONParser(); // JSONParser -> JSON 데이터를 파싱하는 기능을 구현한 클래스
+        JSONObject jsonObject; // JSONObject -> JSON 객체를 추상화한 클래스
+
+        // json 파싱하는 작업이 정상적으로 동작하지 않는 경우를 핸들링하기 위해 예외처리 진행
+        try {
+            jsonObject = (JSONObject) jsonParser.parse(jsonString);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+
+        Map<String, Object> resultMap = new HashMap<>();
+
+        JSONObject mainData = (JSONObject) jsonObject.get("main");
+        resultMap.put("temp", mainData.get("temp"));
+
+        JSONArray weatherArray = (JSONArray) jsonObject.get("weather");
+        JSONObject weatherData = (JSONObject) weatherArray.get(0);
+        resultMap.put("main", weatherData.get("main"));
+        resultMap.put("icon", weatherData.get("icon"));
+
+        return resultMap;
     }
 }
